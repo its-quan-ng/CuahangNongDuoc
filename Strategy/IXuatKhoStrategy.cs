44fbf4d7343ffc4dd30385e9ef00c08adc7a3231
CrossZEvolt <trilegend0@gmail.com>
2025-11-18 09:58:54 +0700

    .

--- Strategy/IXuatKhoStrategy.cs
+++ Strategy/IXuatKhoStrategy.cs
@@ -4,17 +4,10 @@ using CuahangNongduoc.BusinessObject;
 
 namespace CuahangNongduoc.Strategy
 {
-    /// <summary>
-    /// Định nghĩa chiến lược chọn các lô hàng để xuất kho.
-    /// </summary>
+   
     public interface IXuatKhoStrategy
     {
-        /// <summary>
-        /// Chọn các lô hàng để xuất kho theo thuật toán cụ thể.
-        /// </summary>
-        /// <param name="idSanPham">ID sản phẩm cần xuất.</param>
-        /// <param name="soLuongCanXuat">Số lượng cần xuất.</param>
-        /// <returns>Danh sách các mã sản phẩm được chọn, theo thứ tự xuất.</returns>
+       
         IList<MaSanPham> ChonLoXuat(int idSanPham, int soLuongCanXuat);
     }
 }
