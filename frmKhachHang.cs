aed78b91bbe566ee93d1137d3125467393fa6469
Duy Nam <Duynamtran204@gmail.com>
2025-11-20 12:23:51 +0700

    xuLyXoaofrmKhachHangVaDaiLy

--- frmKhachHang.cs
+++ frmKhachHang.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Linq;
 
 namespace CuahangNongduoc
 {
@@ -68,7 +69,25 @@ namespace CuahangNongduoc
 
         private void bindingNavigatorDeleteItem_Click(object sender, EventArgs e)
         {
-            if (MessageBox.Show("Bạn có chắc chắn xóa không?", "Dai Ly", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            DataRowView currentRow = (DataRowView)bindingNavigator.BindingSource.Current;
+            if (currentRow == null)
+                return;
+
+            int idKhachHang = Convert.ToInt32(currentRow["ID"]);
+
+            // Kiểm tra liên kết trước khi xóa
+            if (ctrl.KiemTraLienKet(idKhachHang))
+            {
+                List<string> danhSachBang = ctrl.LayDanhSachBangLienKet(idKhachHang);
+                string thongBao = "Không thể xóa khách hàng này vì đã được sử dụng trong:\n";
+                thongBao += string.Join("\n", danhSachBang.Select(bang => "• " + bang));
+                thongBao += "\n\nVui lòng xóa dữ liệu liên quan trước khi xóa khách hàng này.";
+
+                MessageBox.Show(thongBao, "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (MessageBox.Show("Bạn có chắc chắn xóa không?", "Khách hàng", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
             {
                 bindingNavigator.BindingSource.RemoveCurrent();
             }
@@ -76,7 +95,29 @@ namespace CuahangNongduoc
 
         private void dataGridView_UserDeletingRow(object sender, DataGridViewRowCancelEventArgs e)
         {
-            if (MessageBox.Show("Bạn có chắc chắn xóa không?", "Khach Hang", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+            DataRowView rowView = (DataRowView)e.Row.DataBoundItem;
+            if (rowView == null)
+            {
+                e.Cancel = true;
+                return;
+            }
+
+            int idKhachHang = Convert.ToInt32(rowView["ID"]);
+
+            // Kiểm tra liên kết trước khi xóa
+            if (ctrl.KiemTraLienKet(idKhachHang))
+            {
+                List<string> danhSachBang = ctrl.LayDanhSachBangLienKet(idKhachHang);
+                string thongBao = "Không thể xóa khách hàng này vì đã được sử dụng trong:\n";
+                thongBao += string.Join("\n", danhSachBang.Select(bang => "• " + bang));
+                thongBao += "\n\nVui lòng xóa dữ liệu liên quan trước khi xóa khách hàng này.";
+
+                MessageBox.Show(thongBao, "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                e.Cancel = true;
+                return;
+            }
+
+            if (MessageBox.Show("Bạn có chắc chắn xóa không?", "Khách hàng", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
             {
                 e.Cancel = true;
             }
