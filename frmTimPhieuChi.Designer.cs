04201bf8164785bfae78671792c20a3cb9ecd218
Duy Nam <Duynamtran204@gmail.com>
2025-11-21 16:44:43 +0700

    xuLyTimKiemPhieuChi

--- frmTimPhieuChi.Designer.cs
+++ frmTimPhieuChi.Designer.cs
@@ -32,25 +32,29 @@
             this.cmbLyDo = new System.Windows.Forms.ComboBox();
             this.btnCancel = new System.Windows.Forms.Button();
             this.btnOK = new System.Windows.Forms.Button();
-            this.dtNgayChi = new System.Windows.Forms.DateTimePicker();
+            this.dtTuNgay = new System.Windows.Forms.DateTimePicker();
             this.label2 = new System.Windows.Forms.Label();
+            this.dtDenNgay = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(18, 26);
+            this.label1.Location = new System.Drawing.Point(13, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.Size = new System.Drawing.Size(71, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Lý do chi";
             // 
             // cmbLyDo
             // 
             this.cmbLyDo.FormattingEnabled = true;
-            this.cmbLyDo.Location = new System.Drawing.Point(85, 25);
+            this.cmbLyDo.Location = new System.Drawing.Point(128, 18);
+            this.cmbLyDo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.cmbLyDo.Name = "cmbLyDo";
-            this.cmbLyDo.Size = new System.Drawing.Size(178, 21);
+            this.cmbLyDo.Size = new System.Drawing.Size(265, 28);
             this.cmbLyDo.TabIndex = 1;
             // 
             // btnCancel
@@ -58,59 +62,87 @@
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.btnCancel.Image = global::CuahangNongduoc.Properties.Resources.stop;
             this.btnCancel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnCancel.Location = new System.Drawing.Point(145, 91);
+            this.btnCancel.Location = new System.Drawing.Point(218, 140);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(75, 35);
+            this.btnCancel.Size = new System.Drawing.Size(112, 54);
             this.btnCancel.TabIndex = 15;
             this.btnCancel.Text = "Hủy bỏ";
             this.btnCancel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
             // btnOK
             // 
             this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
             this.btnOK.Image = global::CuahangNongduoc.Properties.Resources.Ok;
             this.btnOK.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnOK.Location = new System.Drawing.Point(64, 91);
+            this.btnOK.Location = new System.Drawing.Point(96, 140);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnOK.Name = "btnOK";
-            this.btnOK.Size = new System.Drawing.Size(75, 35);
+            this.btnOK.Size = new System.Drawing.Size(112, 54);
             this.btnOK.TabIndex = 14;
             this.btnOK.Text = "Đồng ý";
             this.btnOK.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnOK.UseVisualStyleBackColor = true;
             // 
-            // dtNgayChi
+            // dtTuNgay
             // 
-            this.dtNgayChi.CustomFormat = "dd/MM/yyyy";
-            this.dtNgayChi.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dtNgayChi.Location = new System.Drawing.Point(85, 50);
-            this.dtNgayChi.Name = "dtNgayChi";
-            this.dtNgayChi.Size = new System.Drawing.Size(105, 20);
-            this.dtNgayChi.TabIndex = 13;
+            this.dtTuNgay.CustomFormat = "dd/MM/yyyy";
+            this.dtTuNgay.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtTuNgay.Location = new System.Drawing.Point(128, 62);
+            this.dtTuNgay.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dtTuNgay.Name = "dtTuNgay";
+            this.dtTuNgay.Size = new System.Drawing.Size(156, 26);
+            this.dtTuNgay.TabIndex = 13;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(20, 57);
+            this.label2.Location = new System.Drawing.Point(30, 67);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.Size = new System.Drawing.Size(67, 20);
             this.label2.TabIndex = 12;
-            this.label2.Text = "Ngày mua";
+            this.label2.Text = "Từ Ngày";
+            // 
+            // dtDenNgay
+            // 
+            this.dtDenNgay.CustomFormat = "dd/MM/yyyy";
+            this.dtDenNgay.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtDenNgay.Location = new System.Drawing.Point(128, 104);
+            this.dtDenNgay.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dtDenNgay.Name = "dtDenNgay";
+            this.dtDenNgay.Size = new System.Drawing.Size(156, 26);
+            this.dtDenNgay.TabIndex = 17;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 111);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 20);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Đến ngày";
             // 
             // frmTimPhieuChi
             // 
             this.AcceptButton = this.btnOK;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnCancel;
-            this.ClientSize = new System.Drawing.Size(278, 138);
+            this.ClientSize = new System.Drawing.Size(417, 212);
+            this.Controls.Add(this.dtDenNgay);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnOK);
-            this.Controls.Add(this.dtNgayChi);
+            this.Controls.Add(this.dtTuNgay);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.cmbLyDo);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "frmTimPhieuChi";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Tim Phieu Chi";
@@ -125,8 +157,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnCancel;
         private System.Windows.Forms.Button btnOK;
-        public System.Windows.Forms.DateTimePicker dtNgayChi;
+        public System.Windows.Forms.DateTimePicker dtTuNgay;
         private System.Windows.Forms.Label label2;
         public System.Windows.Forms.ComboBox cmbLyDo;
+        public System.Windows.Forms.DateTimePicker dtDenNgay;
+        private System.Windows.Forms.Label label3;
     }
 }
