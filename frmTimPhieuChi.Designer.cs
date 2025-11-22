e2f82528f0e844bc826de1609d42736b443d88d2
Nguyen Quan <ngquan6704@gmail.com>
2025-11-22 05:05:17 -0500

    fix missing reference

--- frmTimPhieuChi.Designer.cs
+++ frmTimPhieuChi.Designer.cs
@@ -166,5 +166,6 @@
         public System.Windows.Forms.ComboBox cmbLyDo;
         public System.Windows.Forms.DateTimePicker dtDenNgay;
         private System.Windows.Forms.Label label3;
+        public System.Windows.Forms.DateTimePicker dtNgayChi;
     }
 }
